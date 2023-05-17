4bdab9d84886accecc669d2d3dafd3667e4a3e3d
JLopezcano <j.lopeza.2017@alumnos.urjc.es>
2023-05-17 21:03:10 +0200

    Feature - Wifi Data and graphics treatment
    Implemented new graphs for boxplot with the wifi data, and new wifi arrays to control the wifi caracteristics.

--- servicios/mapeo/mapeosWifi.py
+++ servicios/mapeo/mapeosWifi.py
@@ -34,8 +34,8 @@ def wifiCappedByPosition(wifis, positions):
         for wifi in wifis[indice]:
             for posi in position:
                 auxColumn = []
-                auxTime1 = posi[0] - 0.8 #2.0
-                auxTime2 = posi[0] + 0.8 #2.0
+                auxTime1 = posi[0] - 0.4 #2.0
+                auxTime2 = posi[0] + 0.4 #2.0
                 if (wifi[0] <= auxTime2) and (wifi[0] >= auxTime1):
                     if (position.index(posi) > int(len(position)/2)):
                         a = 0
@@ -78,5 +78,92 @@ def wifiCapByFrequency(wifis):
                 array.append(element)
         newArray.append(array)
         array = []
+        
+    return newArray
+
+def wifisOrdered(wifis):
+    newArray = []
+    numeroMayor = 0
+    
+    for wifi in wifis:
+        for element in wifi:
+            pi = element[-1]
+            if (int(pi.replace("position","")) > numeroMayor):
+                numeroMayor = int(pi.replace("position",""))
+
+    for wifi in wifis:
+        count = 1
+        array = []
+        while count <= numeroMayor:
+            for element in wifi:
+               if "position" + str(count) == element[-1]:
+                    array.append(element)
+            count = count + 1
+        newArray.append(array)
+
+    return newArray
+
+def wifiOrderingPositionsForBoxplot(wifis):
+    newArray = []
+    arrayPosition = []
+    numeroMayor = 0
+    
+    for wifi in wifis:
+        for element in wifi:
+            pi = element[-1]
+            if (int(pi.replace("position","")) > numeroMayor):
+                numeroMayor = int(pi.replace("position",""))
+                
+    count = 1
+    while count <= numeroMayor:
+        arrayPosition = []
+        for wifi in wifis:
+            for element in wifi:
+                if "position" + str(count) == element[-1]:
+                    arrayPosition.append(element)
+        newArray.append(arrayPosition)
+        count = count + 1
+        
+    return newArray
+
+def wifiGetMeanByPosition(wifis):
+    newArray = []
+    array = []
+    value = 0
+    
+    for wifi in wifis:
+        count = 0
+        value = 0
+        array = []
+        for element in wifi:
+            count = count + 1
+            value = element[3] + value
+        mean = value / count
+        array.append(mean)
+        array.append(element[-1])
+        newArray.append(array)
+            
+    return newArray
+
+def wifiMinusPositionMeans(wifis, means):
+    newArray = []
+    array = []
+    aux = []
+    
+    for wifi in wifis:
+        aux = []
+        for element in wifi:
+            array = []
+            for mean in means:
+                count = 0
+                if (mean[1] == element[-1]):
+                    for value in element:
+                        if count == 3:
+                            array.append(value - mean[0])
+                        else:
+                            array.append(value)
+                        count = count + 1
+            aux.append(array)
+        newArray.append(aux)
         
     return newArray
